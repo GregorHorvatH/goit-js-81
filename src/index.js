4e61855617f0e2cbeaf4c39fe1f75852c08d3734
Herhel Horvat <herhel.horvat@teaminternational.com>
2023-03-31 21:35:17 +0300

    module 2.4

--- src/index.js
+++ src/index.js
@@ -1,186 +1,205 @@
-// Example 1 - Базові операції з масивом
-// Створіть масив genres з елементами «Jazz» та «Blues».
-// Додайте «Рок-н-рол» до кінця.
-// Виведіть у консоль перший елемент масиву.
-// Виведіть у консоль останній елемент масиву. Код повинен працювати для масиву довільної довжини.
-// Видаліть перший елемент та виведіть його в консоль.
-// Вставте «Country» та «Reggae» на початок масиву.
+// https://github.com/goitacademy/js-instructor-examples/blob/main/lesson-04/uk.md
 
-// const genres = ['Jazz', 'Blues'];
+// ---------------------------------------------------------------
+// Example 1 - Індекс маси тіла
+// Напиши функцію calcBMI(weight, height) яка розраховує та повертає індекс маси тіла людини.
+// Для цього необхідно розділити вагу в кілограмах на квадрат висоти людини в метрах.
 
-// genres.push('Rock-n-Roll');
+// Вага та висота будуть спеціально передані як рядки.
+// Не цілі числа можуть бути задані у вигляді 24.7 або 24,7,
+// тобто як роздільник дробової частини може бути кома.
 
-// const firstItem = genres[0];
-// const lastItem = genres[genres.length - 1];
+// Індекс маси тіла необхідно округлити до однієї цифри після коми;
 
-// console.log(firstItem);
-// console.log(lastItem);
+// function calcBMI(weight, height) {
+//   const numWeight = parseFloat(weight.replace(',', '.'));
+//   const numHeight = parseFloat(height.replace(',', '.'));
+//   const bmi = numWeight / Math.pow(numHeight, 2);
 
-// console.log(genres.shift());
+//   return parseFloat(bmi.toFixed(1));
+// }
 
-// genres.unshift('Country', 'Reggae');
+// const bmi = calcBMI('88,3', '1.75');
 
-// console.log(genres);
+// console.log(bmi); // 28.8
 
-// Example 2 - Масиви та рядки
-// Напиши скрипт для обчислення площі прямокутника зі сторонами, значення яких зберігаються у змінній values у вигляді рядка. Значення гарантовано розділені пробілом.
+// ---------------------------------------------------------------
+// Example 2 - Найменше з чисел
+// Напиши функцію min(a,b), яка повертає найменше з чисел a та b.
 
-// const values = '8 11';
-// const items = values.split(' ');
-// const item1 = Number(items[0]);
-// const item2 = parseInt(items[1]);
-// const area = item1 * item2;
+// function min(a, b) {
+//   return a < b ? a : b;
+// }
 
-// console.log(area);
+// const min = (a, b) => {
+//   return a < b ? a : b;
+// };
 
-// Example 3 - Перебір масиву
-// Напиши скрипт для перебору масиву fruits циклом for.
-// Для кожного елемента масиву виведи в консоль рядок у форматі
-// номер_елемента: значення_елемента
+// console.log(min(2, 5)); // 2
+// console.log(min(3, -1)); // -1
+// console.log(min(1, 1)); // 1
+
+// ---------------------------------------------------------------
+// Example 3 - Площа прямокутника
+// Напиши функцію getRectArea(dimensions) для обчислення площі прямокутника зі сторонами, значення яких будуть передані до параметра dimensions у вигляді рядка. Значення гарантовано розділені пробілом.
+
+// function getRectArea(dimensions) {
+//   const numbers = dimensions.split(' ');
+
+//   // const a = numbers[0];
+//   // const b = numbers[1];
+//   const [a, b] = numbers;
+
+//   return Number(a) * Number(b);
+// }
+
+// console.log(getRectArea('8 11'));
+
+// ---------------------------------------------------------------
+// Example 4 - Логування елементів
+// Напиши функцію logItems(items), яка отримує масив та використовує цикл for,
+// який для кожного елемента масиву буде виводити в консоль повідомлення у форматі <номер елемента> - <значення елемента>.
 // Нумерація елементів повинна починатися з 1.
 
-// const fruits = ['🍎', '🍇', '🍑', '🍌', '🍋'];
+// Наприклад для першого елемента масиву ['Mango', 'Poly', 'Ajax'] з індексом 0 буде виведено 1 - Mango, а для індексу 2 виведе 3 - Ajax.
 
-// let i = 1;
-// for (const fruit of fruits) {
-//   console.log(`${i++}: ${fruit}`);
+// function logItems(items) {
+//   console.log('---------------------');
+//   for (let i = 0; i < items.length; i += 1) {
+//     console.log(`${i + 1}: ${items[i]}`);
+//   }
 // }
 
-// for (let i = 0; i < fruits.length; i += 1) {
-//   console.log(`${i + 1}: ${fruits[i]}`);
+// logItems(['Mango', 'Poly', 'Ajax']);
+// logItems(['🍎', '🍇', '🍑', '🍌', '🍋']);
+
+// ---------------------------------------------------------------
+// Example 5 - Логування контактів
+// Напиши функцію printContactsInfo(names, phones) яка виводить у консоль ім'я та телефонний номер користувача. У параметри names та phones будуть передані рядки імен та телефонних номерів, розділені комами. Порядковий номер імен та телефонів у рядках вказують на відповідність. Кількість імен та телефонів гарантовано однакова.
+
+// function printContactsInfo(names, phones) {
+//   const namesArr = names.split(',');
+//   const phonesArr = phones.split(',');
+
+//   for (let i = 0; i < namesArr.length; i += 1) {
+//     console.log(`${namesArr[i]}: ${phonesArr[i]}`);
+//   }
 // }
 
-// Example 4 - Масиви та цикли
-// Напиши скрипт, який виводить у консоль ім'я та телефонний номер користувача.
-// У змінних names та phones зберігаються рядки імен та телефонних номерів,
-// розділені комами. Порядковий номер імен та телефонів у рядках вказують на відповідність.
-// Кількість імен та телефонів гарантовано однакова.
-
-// const names = 'Jacob,William,Solomon,Artemis';
-// const phones = '89001234567,89001112233,890055566377,890055566300';
-
-// // 1. text -> array
-// // 2. iterate the array
-// // 3. create template text `Jacob: 89001234567`
-
-// const namesArr = names.split(',');
-// const phonesArr = phones.split(',');
-
-// for (let i = 0; i < namesArr.length; i += 1) {
-//   console.log(`${namesArr[i]}: ${phonesArr[i]}`);
-// }
-
-// Example 5 - Масиви та рядки
-// Напиши скрипт, який виводить у консоль усі слова рядка крім першого і останнього.
-// Результуючий рядок не повинен починатися або закінчуватися символ пробілу.
-// Скрипт повинен працювати для будь-якого рядка.
-
-// const string = 'Welcome to the future';
-
-// 1. text -> array
-// 2. remove first item
-// 3. remove last item
-// 4. array -> text
-
-// const words = string.split(' ');
-
-// words.shift();
-// words.pop();
-
-// const newString = words.join(' ');
-
-// console.log(`"${newString}"`);
-
-// Example 6 - Масиви та рядки
-// Напиши скрипт, який «розгортає» рядок (зворотний порядок букв) і виводить його в консоль.
-
-// const string = 'Welcome to the future'; // abc -> cba
-
-// 1. text -> array of letters
-// 2. reverse the array
-// 3. array -> text
-
-// var. 1
-// const letters = string.split('');
-// const reversedLetters = letters.reverse();
-// const reversedString1 = reversedLetters.join('');
-
-// console.log(reversedString1);
-
-// var. 2
-// const reversedString2 = string.split('').reverse().join('');
-
-// console.log(reversedString2);
-
-// var. 3
-// const letters = string.split('');
-// const reversedLetters = [];
-
-// for (const letter of letters) {
-//   reversedLetters.unshift(letter);
-// }
-
-// const reversedString = reversedLetters.join('');
-
-// console.log(reversedString);
-
-// Example 7 - Сортування масиву із циклом
-// Напиши скрипт сортування масиву рядків в алфавітному порядку за першою літерою елемента.
-
-// const langs = ['python', 'javascript', 'c++', 'haskel', 'php', 'ruby'];
-
-// console.log(
-//   langs.sort(function (a, b) {
-//     const letter1 = a[0];
-//     const letter2 = b[0];
-
-//     return letter1.localeCompare(letter2);
-//   })
+// printContactsInfo(
+//   'Jacob,William,Solomon,Artemis',
+//   '89001234567,89001112233,890055566377,890055566300'
 // );
 
-// Example 8 - Пошук елемента
-// Напиши скрипт пошуку найменшого числа у масиві.
-// Код повинен працювати для будь-якого масиву чисел.
-// Використовуй цикл для розв'язання задачі.
+// ---------------------------------------------------------------
+// Example 6 - Пошук найбільшого елемента
+// Напиши функцію findLargestNumber(numbers)яка шукає найбільше число в масиві.
 
-// const numbers = [2, 17, 94, 1, 23, 37];
-// let min = numbers[0];
-// let max = numbers[0];
+// function findLargestNumber(numbers) {
+//   let max = numbers[0];
 
-// for (const number of numbers) {
-//   if (number < min) {
-//     min = number;
+//   for (const number of numbers) {
+//     if (number > max) {
+//       max = number;
+//     }
 //   }
 
-//   if (number > max) {
-//     max = number;
-//   }
+//   return max;
 // }
 
-// console.log(min);
-// console.log(max);
+// console.log(findLargestNumber([-2, -17, -94, -1, -23, -37])); // -1
+// console.log(findLargestNumber([2, 17, 94, 1, 23, 37])); // 94
+// console.log(findLargestNumber([49, 4, 7, 83, 12])); // 83
 
-// ---------------- i++ (post increment) vs i += 1 vs ++i (pre increment) ----------------
-// let x = 1;
+// ---------------------------------------------------------------
+// Example 7 - Середнє значення
+// Напишіть функцію calAverage() яка приймає довільну кількість аргументів і повертає їхнє середнє значення. Усі аргументи будуть лише числами.
 
-// console.log((x += 1)); // 2
-// console.log((x += 1)); // 3
-// console.log((x += 1)); // 4
-// console.log((x += 1)); // 5
-// console.log(x); // 5
+// 1. - sum
+// 1.1 - create a variable const sum = 0;
+// 1.2 - for of on the array
+// 1.3 - + array item to the sum
+// 2. - sum / count
+// function calAverage() {
+//   let sum = 0;
 
-// let y = 1;
+//   for (const number of arguments) {
+//     sum += number;
+//   }
 
-// console.log(y++); // 1
-// console.log(y++); // 2
-// console.log(y++); // 3
-// console.log(y++); // 4
-// console.log(y); // 5
+//   return sum / arguments.length;
+// }
 
-// let q = 1;
+// console.log(calAverage(1, 2, 3, 4)); // 2.5
+// console.log(calAverage(14, 8, 2)); // 8
+// console.log(calAverage(27, 43, 2, 8, 36)); // 23.2
 
-// console.log(++q); // 2
-// console.log(++q); // 3
-// console.log(++q); // 4
-// console.log(++q); // 5
-// console.log(q); // 5
+// ---------------------------------------------------------------
+// Example 8 - Форматування часу
+// Напиши функцію formatTime(minutes) яка переведе значення minutes (кількість хвилин) у рядок у форматі годин та хвилин HH:MM.
+
+// function formatTime(minutesInput) {
+//   const hours = Math.floor(minutesInput / 60);
+//   const minutes = minutesInput % 60;
+
+//   const formattedHours = String(hours).padStart(2, 0);
+//   const formattedMinutes = String(minutes).padStart(2, 0);
+
+//   return `${formattedHours}:${formattedMinutes}`;
+// }
+
+// console.log(formatTime(70)); // "01:10"
+// console.log(formatTime(450)); // "07:30"
+// console.log(formatTime(1441)); // "24:01"
+
+// ---------------------------------------------------------------
+// Example 9 - Колекція курсів (includes, indexOf, push і т. д.)
+// Напишіть функції для роботи з колекцією навчальних курсів courses:
+
+// addCourse(name) - додає курс до кінця колекції
+// removeCourse(name) - видаляє курс із колекції
+// updateCourse(oldName, newName) - змінює ім'я на нове
+
+// const courses = ['HTML', 'CSS', 'JavaScript', 'React', 'PostgreSQL'];
+
+// const addCourse = (name) => {
+//   if (courses.includes(name)) {
+//     console.log(`Ви вже маєте курс ${name}`);
+//     return;
+//   }
+
+//   courses.push(name);
+// };
+
+// const removeCourse = (name) => {
+//   if (!courses.includes(name)) {
+//     console.log(`Курс із ім'ям ${name} не знайдено`);
+//     return;
+//   }
+
+//   const idx = courses.indexOf(name);
+//   courses.splice(idx, 1);
+// };
+
+// const updateCourse = (oldName, newName) => {
+//   if (!courses.includes(oldName)) {
+//     console.log(`Курс із ім'ям ${oldName} не знайдено`);
+//     return;
+//   }
+
+//   const idx = courses.indexOf(oldName);
+//   courses.splice(idx, 1, newName);
+// };
+
+// addCourse('Express');
+// console.log(courses); // ['HTML', 'CSS', 'JavaScript', 'React', 'PostgreSQL', 'Express']
+// addCourse('CSS'); // 'Ви вже маєте такий курс'
+
+// removeCourse('React');
+// console.log(courses); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'Express']
+// removeCourse('Vue'); // 'Курс із таким ім'ям не знайдено'
+
+// updateCourse('Express', 'NestJS');
+// console.log(courses); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'NestJS']
+
+// updateCourse('Express', 'NestJS');
